cbf9c95eba025d0cc4efee9014a182d75ca7ff62
zjsdut <zjsdut@163.com>
2020-12-23 14:06:22 +0800

    Fix deprecations about icons and NotificationGroup

--- src/name/admitriev/jhelper/configuration/TaskConfigurationType.java
+++ src/name/admitriev/jhelper/configuration/TaskConfigurationType.java
@@ -3,8 +3,8 @@ package name.admitriev.jhelper.configuration;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.configurations.SimpleConfigurationType;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.util.NotNullLazyValue;
+import name.admitriev.jhelper.icons.JhelperIcons;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -15,11 +15,11 @@ public class TaskConfigurationType extends SimpleConfigurationType {
 				"name.admitriev.jhelper.configuration.TaskConfigurationType",
 				"Task",
 				"Task for JHelper",
-				new NotNullLazyValue<Icon>() {
+				new NotNullLazyValue<>() {
 					@NotNull
 					@Override
 					protected Icon compute() {
-						return IconLoader.getIcon("/name/admitriev/jhelper/icons/task.png");
+						return JhelperIcons.TaskIcon;
 					}
 				}
 		);
