86185110403a7d9740f95d4d7d841154d9c0b36b
Jiashu Zou <zjsdut@163.com>
2021-10-17 16:14:14 +0800

    Fix a gitignore pattern issue
    The Windows file system is case-insensitive, so gitignore patterns are
    also case-insensitive.

---
+++ src/name/admitriev/jhelper/icons/JhelperIcons.java
@@ -0,0 +1,10 @@
+package name.admitriev.jhelper.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface JhelperIcons {
+    Icon TaskIcon = IconLoader.getIcon("/name/admitriev/jhelper/icons/task.png", JhelperIcons.class);
+    Icon DeleteTaskActionIcon = IconLoader.getIcon("/name/admitriev/jhelper/icons/delete.png", JhelperIcons.class);
+}
