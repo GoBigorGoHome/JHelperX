436bdcbb013f75d058bfee9c9e60834b0aab5921
Jiashu Zou <zjsdut@163.com>
2021-11-18 17:34:52 +0800

    Use previously created terminal session
    By adapting https://github.com/JetBrains/intellij-community/blob/d547b76f15e2a748c39bcd23411a7f6279be74d1/plugins/sh/src/com/intellij/sh/run/terminal/ShTerminalRunner.java

--- src/name/admitriev/jhelper/actions/SubmitCodeAction.java
+++ src/name/admitriev/jhelper/actions/SubmitCodeAction.java
@@ -6,7 +6,13 @@ import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.terminal.JBTerminalWidget;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 import name.admitriev.jhelper.IDEUtils;
 import name.admitriev.jhelper.components.Configurator;
 import name.admitriev.jhelper.configuration.TaskConfiguration;
@@ -15,7 +21,13 @@ import name.admitriev.jhelper.generation.CodeGenerationUtils;
 import name.admitriev.jhelper.ui.Notificator;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.terminal.ShellTerminalWidget;
+import org.jetbrains.plugins.terminal.TerminalToolWindowFactory;
 import org.jetbrains.plugins.terminal.TerminalView;
 
 
@@ -50,7 +62,13 @@ public class SubmitCodeAction extends BaseAction {
         VirtualFile file = IDEUtils.getBaseDir(project).findFileByRelativePath(configuration.getOutputFile());
         if (file == null)
             throw new NotificationException("Couldn't find output file");
+
         TerminalView terminalView = TerminalView.getInstance(project);
+        ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(TerminalToolWindowFactory.TOOL_WINDOW_ID);
+        if (window == null) {
+            return;
+        }
+
         String url = ((TaskConfiguration) runConfiguration).getUrl();
         String command;
         if (url.startsWith("https://codeforces")) {
@@ -58,10 +76,49 @@ public class SubmitCodeAction extends BaseAction {
         } else {
             command = "oj s --wait=0 --yes " + url + " " + file.getPath();
         }
+
+        ContentManager contentManager = window.getContentManager();
+        String workingDirectory = project.getBasePath();
+        assert workingDirectory != null;
+        Pair<Content, ShellTerminalWidget> pair = getSuitableProcess(contentManager);
         try {
-            terminalView.createLocalShellWidget(project.getBasePath(), "Submit").executeCommand(command);
-        } catch (IOException err) {
-            err.printStackTrace();
+            if (pair == null) {
+                terminalView.createLocalShellWidget(workingDirectory, "Submit", true).executeCommand(command);
+                return;
+            }
+            window.activate(null, false);
+            pair.second.executeCommand(command);
         }
+        catch (IOException err) {
+            throw new NotificationException("Cannot run command: " + command);
+        }
+    }
+
+    private static @Nullable Pair<Content, ShellTerminalWidget> getSuitableProcess(@NotNull ContentManager contentManager) {
+        Content selectedContent = contentManager.findContent("Submit");
+        if (selectedContent != null) {
+            Pair<Content, ShellTerminalWidget> pair = getSuitableProcess(selectedContent);
+            if (pair != null) return pair;
+        }
+
+        return Arrays.stream(contentManager.getContents())
+                .map(SubmitCodeAction::getSuitableProcess)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private static @Nullable Pair<Content, ShellTerminalWidget> getSuitableProcess(@NotNull Content content) {
+        JBTerminalWidget widget = TerminalView.getWidgetByContent(content);
+        if (!(widget instanceof ShellTerminalWidget)) {
+            return null;
+        }
+
+        ShellTerminalWidget shellTerminalWidget = (ShellTerminalWidget)widget;
+        if (!shellTerminalWidget.getTypedShellCommand().isEmpty() || shellTerminalWidget.hasRunningCommands()) {
+            return null;
+        }
+
+        return new Pair<>(content, shellTerminalWidget);
     }
 }
